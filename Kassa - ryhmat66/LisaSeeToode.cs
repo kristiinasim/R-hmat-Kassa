a3efd4cfb10a198aac33ab1612c9736f4ae693ed
KristelEiTea <kristel.siilak@hotmail.com>
2018-03-27 09:58:09 +0300

    Vigane

---
+++ Kassa - ryhmat66/LisaSeeToode.cs
@@ -0,0 +1,9 @@
+﻿namespace Kassa___ryhmat66
+{
+    public class LisaSeeToode
+    {
+        public string Nimi;
+        public int Hind;
+        public int Kogus;
+    }
+}
--- Kassa - ryhmat66/MainWindow.xaml.cs
+++ Kassa - ryhmat66/MainWindow.xaml.cs
@@ -20,28 +20,43 @@ namespace Kassa___ryhmat66
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<LisaSeeToode> items;
+        List<LisaSeeToode> items = new List<LisaSeeToode>();
         public MainWindow()
         {
             InitializeComponent();
-            items = new List<LisaSeeToode>();
+            //List<LisaSeeToode> items = new List<LisaSeeToode>();
             
 
-            items.Add(new LisaSeeToode() { Nimi = "aferg", Hind = 7, Kogus = 1 });
+            items.Add(new LisaSeeToode() { Nimi = "Piim", Hind = 45, Kogus = 10 });
+
+            items.Add(new LisaSeeToode() { Nimi = "Perenaise Sai", Hind = 80, Kogus = 12 });
+
+            items.Add(new LisaSeeToode() { Nimi = "Kange Walter", Hind = 100, Kogus = 5 });
+
+            items.Add(new LisaSeeToode() { Nimi = "Lotte Limonaad", Hind = 10, Kogus = 8 });
+
+            items.Add(new LisaSeeToode() { Nimi = "Maasikad", Hind = 40, Kogus = 30 });
+
             TootedListBox.ItemsSource = items;
+
+            //TootedListBox.ItemsSource = items;
+
+            //items.Add(new LisaSeeToode() { Nimi = "aferg", Hind = 7, Kogus = 1 });
+            //TootedListBox.ItemsSource = items;
         }
 
         private void LisaToode_Click(object sender, RoutedEventArgs e)
         {
-            items.Add(new LisaSeeToode() { Nimi = TooteNimi.Text, Hind = int.Parse(TooteHind.Text), Kogus = int.Parse(TooteKogus.Text) });
-            TootedListBox.ItemsSource = items;
+            //List<LisaSeeToode> items = new List<LisaSeeToode>();
+            //items.Add(new LisaSeeToode() { Nimi = TooteNimi.Text, Hind = int.Parse(TooteHind.Text), Kogus = int.Parse(TooteKogus.Text) });
+            //TootedListBox.ItemsSource = items;
         }
 
-        public class LisaSeeToode
+       
+
+        private void TootedListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            public string Nimi;
-            public int Hind;
-            public int Kogus;
+
         }
     }
 }

63c89044f974e55f54b3e97601b9bba51d0c6aaf
Katrin Kirotarp <Katrin.Kirotarp@outlook.com>
2018-04-23 19:23:07 +0300

    Kustutamine ok, textlahtrid auto-tyhjenemine, ostukorvi fail tühjaks

--- Kassa - ryhmat66/MainWindow.xaml.cs
+++ Kassa - ryhmat66/MainWindow.xaml.cs
@@ -33,11 +33,13 @@ namespace Kassa___ryhmat66
             string tekst = "Ostukorvis on: ";
             File.AppendAllText(path, tekst);
             path = @"..\..\..\list.txt";
+            File.Create(path).Close();
+
         }
 
         private void LisaToode_Click(object sender, RoutedEventArgs e)
         {
-            int n;
+            int n=0;
             bool HindIsNumeric = int.TryParse(TooteHind.Text, out n);
             bool KogusIsNumeric = int.TryParse(TooteKogus.Text, out n);
             if (TooteNimi.Text != null && TooteHind.Text != null && HindIsNumeric == true 
@@ -76,14 +78,82 @@ namespace Kassa___ryhmat66
                 string KoguSumma = System.Environment.NewLine + "Kogu summa on: " + Summa + "€";
                 File.AppendAllText(path, KoguSumma);
                 MessageBox.Show("Toode on lisatud ostukorvi!");
+            } else
+            {
+                MessageBox.Show(
+                    "Vali toode enne kui lisad ostukorvi",
+                    "Ostukorvi viga!",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error
+                    );
             }
         }
 
         private void Vaata_Click(object sender, RoutedEventArgs e)
         {
             Process p = new Process();
-            Process.Start(@"..\..\..\kassa.txt");
+            Process.Start(path);
             p.Close();
         }
+
+        private void Eemalda_Click(object sender, RoutedEventArgs e)
+        {
+            if (TootedListBox.SelectedItem != null)
+            {
+                string text = System.Environment.NewLine + "Eemaldasite: " + TooteNimi.Text + " Toote hind: " + TooteHind.Text + "€ Toote kogus: 1";
+                File.AppendAllText(path, text);
+                int ToodeteKoguHind = int.Parse(TooteHind.Text);
+                int Summa = Algmaksumus -= ToodeteKoguHind;
+                string KoguSumma = System.Environment.NewLine + "Kogu summa on: " + Summa + "€";
+                File.AppendAllText(path, KoguSumma);
+                MessageBox.Show("Toode on eemaldatud ostukorvist!");
+            } else
+            {
+                MessageBox.Show(
+                    "Valige toode, mida soovite eemaldada!",
+                    "!!!",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error
+                    );
+            }
+        }
+
+        private void TooteNimi_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if(TooteNimi.Text == "Toote nimi")
+                TooteNimi.Text = "";
+        }
+
+        private void TooteNimi_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (TooteNimi.Text == "")
+                TooteNimi.Text = "Toote nimi";
+        }
+
+        
+
+        private void Hind_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (TooteHind.Text == "")
+                TooteHind.Text = "Hind";
+        }
+
+        private void Hind_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (TooteHind.Text == "Hind")
+                TooteHind.Text = "";
+        }
+        private void Kogus_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (TooteKogus.Text == "Kogus")
+                TooteKogus.Text = "";
+        }
+
+        private void Kogus_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (TooteKogus.Text == "")
+                TooteKogus.Text = "Kogus";
+        }
+
     }
 }

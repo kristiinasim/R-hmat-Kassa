5bdbff16f874c59f9612523b8d4ab59f66d9fca5
Katrin Kirotarp <Katrin.Kirotarp@outlook.com>
2018-04-25 19:11:31 +0300

    Lao seisu salvestamine faili, koodi puhastamine

--- Kassa - ryhmat66/MainWindow.xaml.cs
+++ Kassa - ryhmat66/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.IO;
 using System.Diagnostics;
 using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,65 +24,101 @@ namespace Kassa___ryhmat66
     /// </summary>
     public partial class MainWindow : Window
     {
-        public string path;
+        public string failOstukorv;
+        public string failLaoseis;
         public int Algmaksumus = 0;
         List<LisaSeeToode> items = new List<LisaSeeToode>();
+
+        private string appTitle = "Kassa";
+        private string AppTooteNimi = "Toote nimi";
+        private string AppTooteHind = "Hind";
+        private string AppTooteKogus = "Kogus";
+
         public MainWindow()
         {
             InitializeComponent();
-            path = @"..\..\..\kassa.txt";
-            File.Delete(path);
+
+            TooteHind.Text = AppTooteHind;
+            TooteKogus.Text = AppTooteKogus;
+            TooteNimi.Text = AppTooteNimi;
+            appWindow.Title = appTitle;
+
+            failLaoseis = @"..\..\..\laoseis.txt";
+            failOstukorv = @"..\..\..\ostukorv.txt";
+
+            try
+            {
+                items = DeSerializeObject<List<LisaSeeToode>>(failLaoseis);
+                if (items == null) { items = new List<LisaSeeToode>(); }
+                TootedListBox.ItemsSource = items;
+                TootedListBox.Items.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error : " + ex.Message, appTitle);
+                // logi veateade, kui on vaja
+            }
+
+
+            File.Delete(failOstukorv);
             string tekst = "Ostukorvis on: ";
-            File.AppendAllText(path, tekst);
-            path = @"..\..\..\list.txt";
-            File.Create(path).Close();
+            File.AppendAllText(failOstukorv, tekst);
+            File.Create(failOstukorv).Close();
 
         }
 
+
+
+        /*
+         * Lisa toote nupu vajutus.
+         * 
+         * Lisab tekstiväljadele sisestatud toote info tooodete nimekirja.
+         * 
+         */
         private void LisaToode_Click(object sender, RoutedEventArgs e)
         {
-            int n=0;
-            bool HindIsNumeric = int.TryParse(TooteHind.Text, out n);
+            bool HindIsNumeric = int.TryParse(TooteHind.Text, out int n);
             bool KogusIsNumeric = int.TryParse(TooteKogus.Text, out n);
-            if (TooteNimi.Text != null && TooteHind.Text != null && HindIsNumeric == true 
+            if (TooteNimi.Text != null && TooteHind.Text != null && HindIsNumeric == true
                 && TooteKogus.Text != null && KogusIsNumeric == true)
             {
                 items.Add(new LisaSeeToode() { Nimi = TooteNimi.Text, Hind = int.Parse(TooteHind.Text), Kogus = int.Parse(TooteKogus.Text) });
                 TootedListBox.ItemsSource = items;
-                string message = string.Format("Listis on nüüd: " + TooteNimi.Text + "; Hind: " + TooteHind.Text + " € Kogus: " + TooteKogus.Text);
-                MessageBox.Show(message);
+                string message = string.Format("Lisasime:\n\tToode : " + TooteNimi.Text + "\n\tHind : " + TooteHind.Text + " €\n\tKogus : " + TooteKogus.Text);
+                MessageBox.Show(message, appTitle);
                 TootedListBox.Items.Refresh();
             }
             else
             {
                 string errormessage = "Palun täida kõik lahtrid õigesti";
-                MessageBox.Show(errormessage);
+                MessageBox.Show(errormessage, appTitle);
             }
-                
+
 
         }
 
-       
 
-        private void TootedListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
+        /**
+         * Ostukorvi nupule vajutus.
+         * 
+         * Ostukorvi nupule vajutades teeme ...
+         * 
+         */
         private void Ostukorvi_Click(object sender, RoutedEventArgs e)
         {
-            if (TootedListBox.SelectedItem != null)
+            if (TootedListBox.SelectedItem != null) // Kui oli midagi valitud, siis lisame ostukorvi
             {
-                string NimiTootel = (TootedListBox.SelectedItem as LisaSeeToode).Nimi;
-                int Price = (TootedListBox.SelectedItem as LisaSeeToode).Hind;
-                string text = System.Environment.NewLine + "Toote nimi: " + NimiTootel + " Toote hind: " + Price + "€ Toote kogus: 1";
-                File.AppendAllText(path, text);
+                string ToodeNimi = (TootedListBox.SelectedItem as LisaSeeToode).Nimi;
+                int ToodeHind = (TootedListBox.SelectedItem as LisaSeeToode).Hind;
+                string text = System.Environment.NewLine + "Toote nimi: " + ToodeNimi + " Toote hind: " + ToodeHind + "€ Toote kogus: 1";
+                File.AppendAllText(failOstukorv, text);
                 int ToodeteKoguHind = int.Parse(TooteHind.Text);
                 int Summa = Algmaksumus += ToodeteKoguHind;
                 string KoguSumma = System.Environment.NewLine + "Kogu summa on: " + Summa + "€";
-                File.AppendAllText(path, KoguSumma);
-                MessageBox.Show("Toode on lisatud ostukorvi!");
-            } else
+                File.AppendAllText(failOstukorv, KoguSumma);
+                MessageBox.Show("Toode on lisatud ostukorvi!", appTitle);
+            }
+            else
             {
                 MessageBox.Show(
                     "Vali toode enne kui lisad ostukorvi",
@@ -94,7 +132,7 @@ namespace Kassa___ryhmat66
         private void Vaata_Click(object sender, RoutedEventArgs e)
         {
             Process p = new Process();
-            Process.Start(path);
+            Process.Start(failOstukorv);
             p.Close();
         }
 
@@ -105,17 +143,18 @@ namespace Kassa___ryhmat66
                 string NimiTootel = (TootedListBox.SelectedItem as LisaSeeToode).Nimi;
                 int Price = (TootedListBox.SelectedItem as LisaSeeToode).Hind;
                 string text = System.Environment.NewLine + "Eemaldasite: " + NimiTootel + " Toote hind: " + Price + "€ Toote kogus: 1";
-                File.AppendAllText(path, text);
+                File.AppendAllText(failOstukorv, text);
                 int ToodeteKoguHind = int.Parse(TooteHind.Text);
                 int Summa = Algmaksumus -= ToodeteKoguHind;
                 string KoguSumma = System.Environment.NewLine + "Kogu summa on: " + Summa + "€";
-                File.AppendAllText(path, KoguSumma);
-                MessageBox.Show("Toode on eemaldatud ostukorvist!");
-            } else
+                File.AppendAllText(failOstukorv, KoguSumma);
+                MessageBox.Show("Toode on eemaldatud ostukorvist!", appTitle);
+            }
+            else
             {
                 MessageBox.Show(
                     "Valige toode, mida soovite eemaldada!",
-                    "!!!",
+                    appTitle,
                     MessageBoxButton.OK,
                     MessageBoxImage.Error
                     );
@@ -124,40 +163,119 @@ namespace Kassa___ryhmat66
 
         private void TooteNimi_GotFocus(object sender, RoutedEventArgs e)
         {
-            if(TooteNimi.Text == "Toote nimi")
+            if (TooteNimi.Text == AppTooteNimi)
                 TooteNimi.Text = "";
         }
 
         private void TooteNimi_LostFocus(object sender, RoutedEventArgs e)
         {
             if (TooteNimi.Text == "")
-                TooteNimi.Text = "Toote nimi";
+                TooteNimi.Text = AppTooteNimi;
         }
 
-        
+
 
         private void Hind_LostFocus(object sender, RoutedEventArgs e)
         {
             if (TooteHind.Text == "")
-                TooteHind.Text = "Hind";
+                TooteHind.Text = AppTooteHind;
         }
 
         private void Hind_GotFocus(object sender, RoutedEventArgs e)
         {
-            if (TooteHind.Text == "Hind")
+            if (TooteHind.Text == AppTooteHind)
                 TooteHind.Text = "";
         }
         private void Kogus_GotFocus(object sender, RoutedEventArgs e)
         {
-            if (TooteKogus.Text == "Kogus")
+            if (TooteKogus.Text == AppTooteKogus)
                 TooteKogus.Text = "";
         }
 
         private void Kogus_LostFocus(object sender, RoutedEventArgs e)
         {
             if (TooteKogus.Text == "")
-                TooteKogus.Text = "Kogus";
+                TooteKogus.Text = AppTooteKogus;
         }
 
+
+        /// <summary>
+        /// Serialiseeri objekt faili
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="serializableObject"></param>
+        /// <param name="fileName"></param>
+        public void SerializeObject<T>(T serializableObject, string fileName)
+        {
+            if (serializableObject == null) { return; }
+
+            try
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                XmlSerializer serializer = new XmlSerializer(serializableObject.GetType());
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    serializer.Serialize(stream, serializableObject);
+                    stream.Position = 0;
+                    xmlDocument.Load(stream);
+                    xmlDocument.Save(fileName);
+                    stream.Close();
+                }
+            }
+            catch (Exception)
+            {
+                // Logi exception siin, kui vaja
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public T DeSerializeObject<T>(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName)) { return default(T); }
+
+            T objectOut = default(T);
+
+            try
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(fileName);
+                string xmlString = xmlDocument.OuterXml;
+
+                using (StringReader read = new StringReader(xmlString))
+                {
+                    Type outType = typeof(T);
+                    XmlSerializer serializer = new XmlSerializer(outType);
+                    using (XmlReader reader = new XmlTextReader(read))
+                    {
+                        objectOut = (T)serializer.Deserialize(reader);
+                        reader.Close();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // Logi Exception, kui on vajadus
+            }
+
+            return objectOut;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            try
+            {
+                SerializeObject<List<LisaSeeToode>>(items, failLaoseis);
+            }
+            catch (Exception)
+            {
+
+            }
+            MessageBox.Show("Täname et kasutasite meie programmi!\nPeatse taasnägemiseni!", appTitle, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
     }
 }
